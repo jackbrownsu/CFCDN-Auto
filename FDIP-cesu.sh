e0c9388b0a2ef863ea7e02a7e0ed264eed4c1fe7
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-06-25 13:15:46 +0800

    Update FDIP-cesu.sh

--- FDIP-cesu.sh
+++ FDIP-cesu.sh
@@ -5,14 +5,17 @@ export LANG=zh_CN.UTF-8
 
 # 配置目录
 BASE_DIR=$(pwd)
+FDIP_DIR="${BASE_DIR}/FDIP"
 CFST_DIR="${BASE_DIR}/CloudflareST"
-IP_DIR="${BASE_DIR}/FDIP"
-IP_DIR_CLEAN="${BASE_DIR}/FDIPC"
 
-# 创建所需目录
-mkdir -p "${CFST_DIR}"
-mkdir -p "${IP_DIR}"
-mkdir -p "${IP_DIR_CLEAN}"
+# 检查并创建所需目录
+if [ ! -d "$FDIP_DIR" ]; then
+    mkdir -p "$FDIP_DIR"
+fi
+
+if [ ! -d "$CFST_DIR" ]; then
+    mkdir -p "$CFST_DIR"
+fi
 
 # 更新并安装依赖
 install_dependency() {
@@ -39,68 +42,41 @@ fi
 
 echo "============================开始下载白嫖的反代IP包============================="
 download_url="https://zip.baipiao.eu.org/"
-save_path="${CFST_DIR}/txt.zip"
-extracted_folder="${CFST_DIR}/FDIP-TXT"
+save_path="${FDIP_DIR}/txt.zip"
 
 # 尝试下载并解压文件
 for attempt in {1..10}; do
-    wget "${download_url}" -O $save_path && break || echo "下载尝试 $attempt 失败。"
+    wget "${download_url}" -O "$save_path" && break || echo "下载尝试 $attempt 失败。"
 done
 
-rm -rf $extracted_folder/*
-unzip $save_path -d $extracted_folder
+rm -rf "$FDIP_DIR"/*.txt
+unzip "$save_path" -d "$FDIP_DIR"
 sleep 1
 
 echo "===============================合并反代IP文件==============================="
-temp_file="${IP_DIR}/FDIPtemp.txt"
-cat $extracted_folder/${asn}*${port}.txt > $temp_file
+temp_file="${FDIP_DIR}/FDIPtemp.txt"
+cat "${FDIP_DIR}"/*"${port}".txt > "$temp_file"
 
 echo "===============================IP去重==============================="
-awk '!seen[$0]++' $temp_file > "${IP_DIR}/FDIPtemp_unique.txt"
+awk '!seen[$0]++' "$temp_file" > "${FDIP_DIR}/FDIPtemp_unique.txt"
 
-#######################################验证反代IP及纯净度###########################################
-FDIP="${IP_DIR}/FDIP.txt"
-FDIPC="${IP_DIR_CLEAN}/FDIPC.txt"
-> $FDIP
-> $FDIPC
-
-if [ "$scip" = "true" ]; then
-    echo "已选验证反代IP及纯净度"
-    while IFS= read -r ip; do
-        PAGE_CONTENT=$(curl -s --connect-timeout 1 "http://$ip")
-        if echo "$PAGE_CONTENT" | grep -q "1003"; then
-            echo "$ip" >> "$FDIP"
-            echo "得到一个反代IP[$ip]"
-            if curl -s --connect-timeout 1 "https://scamalytics.com/ip/$ip" | grep -q '"risk":"low"'; then
-                echo "$ip" >> "$FDIPC"
-                echo "此反代IP纯净[$ip]"
-            fi
-        fi
-    done < "${IP_DIR}/FDIPtemp_unique.txt"
-else
-    cat "${IP_DIR}/FDIPtemp_unique.txt" > $FDIP
-    echo "未选验证反代IP及纯净度"
-fi
-
-echo "==============================开始识别IP所在地==============================="
-while read -r ip; do
-    country_code=$(mmdblookup --file "${CFST_DIR}/Country.mmdb" --ip $ip country iso_code | awk -F '"' '{print $2}')
-    echo $ip >> "${IP_DIR}/${country_code}-${port}.txt"
-done < $FDIP
-
-while read -r ip; do
-    country_code=$(mmdblookup --file "${CFST_DIR}/Country.mmdb" --ip $ip country iso_code | awk -F '"' '{print $2}')
-    echo $ip >> "${IP_DIR_CLEAN}/${country_code}-${port}.txt"
-done < $FDIPC
-
+# 删除已下载的txt.zip及其解压出来的txt文件
 rm -rf $extracted_folder $save_path
 
-echo "IP所在地识别完毕，结果已储存在${IP_DIR}文件夹中，纯净IP文件夹为${IP_DIR_CLEAN}"
+echo "=========================筛选国家代码为SG的IP地址=========================="
+sg_file="${FDIP_DIR}/sg.txt"
+> $sg_file
+
+while IFS= read -r ip; do
+    country_code=$(mmdblookup --file "${CFST_DIR}/Country.mmdb" --ip $ip country iso_code | awk -F '"' '{print $2}')
+    if [ "$country_code" == "SG" ]; then
+        echo $ip >> $sg_file
+    fi
+done < $temp_file
 
 ########################## 开始测速并过滤 ##########################
-SG_FILE="${IP_DIR_CLEAN}/SG-443.txt"
-OUTPUT_FILE="${IP_DIR}/SG443FD.csv"
-FINAL_OUTPUT="${IP_DIR}/SG_filtered.txt"
+OUTPUT_FILE="${FDIP_DIR}/SG443FD.csv"
+FINAL_OUTPUT="${FDIP_DIR}/sgcs.txt"
 URL="https://spurl.api.030101.xyz/100mb"
 
 # 下载 CloudflareSpeedTest
@@ -109,7 +85,7 @@ tar -xzf "${CFST_DIR}/CloudflareST.tar.gz" -C "${CFST_DIR}"
 chmod +x "${CFST_DIR}/CloudflareST"
 
 # 运行速度测试
-"${CFST_DIR}/CloudflareST" -tp 443 -f $SG_FILE -n 500 -dn 8 -tl 250 -tll 10 -o $OUTPUT_FILE -url $URL
+"${CFST_DIR}/CloudflareST" -tp 443 -f $sg_file -n 500 -dn 8 -tl 250 -tll 10 -o $OUTPUT_FILE -url $URL
 
 # 过滤速度高于8ms/s的IP
 awk -F, 'NR>1 && $7 > 8 {print $1 "#" $2 "-" $7 "mb/s"}' $OUTPUT_FILE > $FINAL_OUTPUT
